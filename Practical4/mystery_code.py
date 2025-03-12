8668d6884d145264032965f5e915c495662dfd7e
yuming333 <yuming.24@intl.zju.edu.cngit config --global user.email yuming.24@intl.zju.edu.cn git config --global user.name yuming333git config --global user.name yuming33>
2025-03-12 15:58:25 +0100

    Add comments and answer to mystery_code.py

---
+++ Practical4/mystery_code.py
@@ -0,0 +1,20 @@
+# What does this piece of code do?
+# Answer: This Python code calculates the number of attempts required to generate two identical random integers within a specified range.  
+# Import libraries
+# randint allows drawing a random number,
+# e.g. randint(1,5) draws a number between 1 and 5
+from random import randint
+
+# ceil takes the ceiling of a number, i.e. the next higher integer.
+# e.g. ceil(4.2)=5
+from math import ceil
+
+progress=0
+while progress>=0:
+	progress+=1
+	first_n = randint(1,6)
+	second_n = randint(1,6)
+	if first_n == second_n:
+		print(progress)
+		break
+
