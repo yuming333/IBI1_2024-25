e8c2b5c9c9b666f3666c75f4da29541a67d2a7ea
yuming333 <yuming.24@intl.zju.edu.cngit config --global user.email yuming.24@intl.zju.edu.cn git config --global user.name yuming333git config --global user.name yuming33>
2025-03-12 16:30:26 +0100

    bmi_calculator.py uploaded

---
+++ Practical4/bmi_calculator.py
@@ -0,0 +1,32 @@
+# Step 1: Get the weight (in kg) and height (in m) of the person
+
+# Step 2: Calculate the BMI using the formula: BMI = weight / (height ** 2)
+
+# Step 3: Determine the category based on BMI(using "if,elif,else"):
+# - BMI < 18.5: Underweight
+# - 18.5 <= BMI <= 30: Normal weight
+# - BMI > 30: Obese
+
+# Step 4: Print the result, which includes the BMI and category
+# BMI Calculator
+
+
+# Step 1:Get the weight (in kg) and height (in m) of the person
+weight = float(input("Enter your weight in kg: "))
+height = float(input("Enter your height in meters: "))
+
+
+# Step 2: Calculate the BMI using the formula: BMI = weight / (height ** 2)
+bmi = round(weight / (height ** 2), 2)
+
+
+# Step 3: Determine the category based on BMI:
+if bmi < 18.5:
+    category = "underweight"
+elif 18.5 <= bmi <= 30:
+    category = "normal weight"
+else:
+    category = "obese"
+
+# Step 4: Print the result, which includes the BMI and category
+print("Your BMI is " + str(bmi) + ", which means you are " + category + ".")
