9105cf1f5d94b4a2b27fd9fe4aeb0b2f9a9308bb
yuming333 <yuming.24@intl.zju.edu.cn>
2025-03-12 22:32:02 +0800

    Update triangle.py

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,12 +1,13 @@
 triangle_numbers = []
-# Loop from 1 to 10 (since we want to calculate the first ten values)
-for n in range(1, 11):# Initialize the sum of the current triangular number to 0
+# Loop from 1 to 10 to calculate the first ten values
+for n in range(1, 11):
+# Initialize the sum of the current triangular number
     sum = 0
-     # Loop from 1 to n to calculate the sum from 1 to n
+# Loop from 1 to n to calculate the sum from 1 to n
     for i in range(1, n + 1):
-        sum += i# Add the calculated sum to the list
-    triangle_numbers.append(sum)
+        sum += i
+    triangle_numbers.append(sum)# Add the calculated sum to the list
 
-# Iterate through the list and print each triangular number
+# Print each triangular number
 for number in triangle_numbers:
      print(number)
