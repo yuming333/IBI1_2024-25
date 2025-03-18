8489d7ad9fef92a2cba8babbfe7b839ad4ef441d
yuming333 <yuming.24@intl.zju.edu.cngit config --global user.email yuming.24@intl.zju.edu.cn git config --global user.name yuming333git config --global user.name yuming33>
2025-03-18 12:16:43 +0100

    problem1.py updated

--- Practical5/problem1.py
+++ Practical5/problem1.py
@@ -1,23 +1,30 @@
+# Dictionary to store programming language popularity
+language_popularity = { "JavaScript": 62.3, "HTML": 52.9, "Python": 51, "SQL": 51, "TypeScript": 38.5 }
+# Print the dictionary,so it is easy to see if the data is stored properly
+print(language_popularity)
 
-language_data = {"JavaScript": 62.3,"HTML": 52.9,"Python": 51,"SQL": 51,"TypeScript": 38.5}
-print(language_data)
+try:
+    import matplotlib.pyplot as plt
+except ModuleNotFoundError:         # Increase the fault - tolerance rate to prevent the program from malfunctioning in case matplotlib is not properly installed.
+    print("The matplotlib library is not installed.")
+    
+# Get the keys and percentage data from the dictionary created above, and store the data in two separated lists.
+languages = list(language_popularity.keys())
+percentages = list(language_popularity.values())
 
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
+# Draw a bar chart describing the data in the dictionary
+plt.figure(figsize = (10,6))
+plt.bar(languages,percentages,color='skyblue') 
+plt.xlabel('Programming Languages') # Add x axis's label 
+plt.ylabel('Percentage of Users') # Add y axis's label
+plt.title('Programming Language Popularity in February 2024') # Add title of the bar chart
+plt.show() # Display the bar chart
 
-languages = list(language_data.keys())
-percentages = list(language_data.values())
-
-plt.bar(languages, percentages)
-plt.xlabel('Programming Language')
-
-plt.ylabel('Percentage of Users')
-plt.title('Programming Language Popularity in February 2024')
-plt.show()
-
-input_language = "Python" 
-if input_language in language_data:
-    print(f"The percentage of developers who use {input_language} is {language_data[input_language]}%")
+input_language = "Python" # Pick a kind of language from the given chart.A variable	of the requested activity that can be modified.
+# Print the result
+if input_language in language_popularity:
+    print(f"The percentage of developers who use {input_language} is {language_popularity[input_language]}%")
 else:
     print(f"{input_language} is not in the provided data.")
+
+
