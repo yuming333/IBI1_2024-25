2683c854abe536abe643768f539e2d7134826668
yuming333 <yuming.24@intl.zju.edu.cngit config --global user.email yuming.24@intl.zju.edu.cn git config --global user.name yuming333git config --global user.name yuming33>
2025-04-09 12:48:11 +0100

    Practical7

--- Practical7/spliced_tata_genes.py
+++ Practical7/spliced_tata_genes.py
@@ -1,53 +1,55 @@
+
+# import re
 import re
 
-def count_tata_boxes(sequence):
-    """Count the number of TATA boxes in a sequence (case-insensitive)."""
-    return len(re.findall(r'TATA[AT]A[AT][AG]', sequence, re.IGNORECASE))
+# input the one	of three possible splice combinations
+valid_combinations = input("please provide the valid_combinations (ATAC,GCAG,GTAG): ")
 
-def process_genes(splice_combination):
-    input_filename = r'C:/Users/wym/Desktop/wym/IBI1_2024-25/Practical7/tata_genes.fa'
+# set the input and output path and output file name
+input = r'C:/Users/wym/Desktop/wym/IBI1_2024-25/Practical7/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa'
+output = f'C:/Users/wym/Desktop/wym/IBI1_2024-25/Practical7/{valid_combinations}_spliced_genes.fa'
 
-    output_filename = f'{splice_combination}'
-    
-    with open(input_filename, 'r') as infile, open(output_filename, 'w') as outfile:
-        current_gene = None
-        current_sequence = []
+
+# Analyze FASTA file
+genes = {}
+current_gene = None
+with open(input, 'r') as file:
+    for line in file:
+        line = line.strip() # Delete white space
         
-        for line in infile:
-            if line.startswith('>'):
-                # If we have a gene being processed, check it before starting a new one
-                if current_gene is not None and current_sequence:
-                    full_sequence = ''.join(current_sequence)
-                    tata_count = count_tata_boxes(full_sequence)
-                    if tata_count > 0:
-                        # Write to output file in the required format
-                        outfile.write(f'>{current_gene} {tata_count}\n')
-                        outfile.write(f'{full_sequence}\n')
-                
-                # Start new gene
-                current_gene = line[1:].strip().split()[0]  # Get just the gene name
-                current_sequence = []
-            else:
-                current_sequence.append(line.strip())
-        
-        # Process the last gene in the file
-        if current_gene is not None and current_sequence:
-            full_sequence = ''.join(current_sequence)
-            tata_count = count_tata_boxes(full_sequence)
-            if tata_count > 0:
-                outfile.write(f'>{current_gene} {tata_count}\n')
-                outfile.write(f'{full_sequence}\n')
-
-def main():
-    valid_combinations = ['GTAG', 'GCAG', 'ATAC']
-    while True:
-        splice_combination = input(f"Enter one of the splice combinations {valid_combinations}: ").upper()
-        if splice_combination in valid_combinations:
-            break
-        print("Invalid combination. Please try again.")
+        if line.startswith('>'):
+            match = re.search(r'gene:(\S+)', line)
+            if match: 
+                current_gene = match.group(1) 
+                genes[current_gene] = "" 
     
-    process_genes(splice_combination)
-    print(f"Output written to {splice_combination}_spliced_genes.fa")
+        elif current_gene is not None: 
+            genes[current_gene] += line
 
-if __name__ == "__main__":
-    main()
+# Select genes that contain the specified splice site
+keys_to_delete = []
+for gene_name in genes:
+    cut = valid_combinations[:2] + r'.*' + valid_combinations[-2:]  # pattern
+    seqs = re.search(cut, genes[gene_name])
+    if  not seqs:
+        keys_to_delete.append(gene_name)
+
+for key in keys_to_delete:
+    del genes[key]
+
+
+
+# get key and value, filtrate TATA
+tata_genes = {}
+for gene_name in genes:
+    sequence = genes[gene_name]  
+
+    if re.search(r'TATA[AT]A[AT]', sequence):
+        tata_genes[gene_name] = sequence
+
+# output the result
+with open(output, 'w') as file:
+    for gene_name, sequence in tata_genes.items():
+        t = re.findall(r'TATA[AT]A[AT]', sequence)
+        file.write('>' + gene_name + ' :' + str(len(t)) + '\n')
+        file.write(sequence + '\n')
