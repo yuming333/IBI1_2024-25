bcc015e5ebb9872ab918625e393c0dbcd6e491dd
yuming333 <yuming.24@intl.zju.edu.cngit config --global user.email yuming.24@intl.zju.edu.cn git config --global user.name yuming333git config --global user.name yuming33>
2025-05-22 09:36:58 +0100

    Practical14 edited

--- Practical14/GO.py
+++ Practical14/GO.py
@@ -1,124 +1,145 @@
+from xml.dom.minidom import parse as xml_parse
+import xml.dom.minidom as xdm
+import xml.sax as xs
+import time as tm
 
-import xml.dom.minidom
-import xml.sax 
-from datetime import datetime
+# DOM
+begin_time = tm.time() # Time recording
+dom_data = xdm.parse(r"C:/Users/wym/Desktop/wym/IBI1_2024-25/Practical14/go_obo.xml") # Parse the xml file
+root_element = dom_data.documentElement
+all_terms = root_element.getElementsByTagName("term")
 
-# DOM   
-xml_file = r"C:/Users/wym/Desktop/wym/IBI1_2024-25/Practical14/go_obo.xml"    
-start_time = datetime.now() # time record
-DOMTree = xml.dom.minidom.parse(xml_file)# prase file
-collection = DOMTree.documentElement # collect the document elements
-terms = collection.getElementsByTagName("term") # get the tag is term
-# set the max dictionary
-ontology_max = {
-    "molecular_function": ("", "", 0),
-    "biological_process": ("", "", 0),
-    "cellular_component": ("", "", 0)
+# Initialize dictionary for storing results
+ontology_results = {
+    "molecular_function": [],
+    "biological_process": [],
+    "cellular_component": []
 }
-for term in terms:
-    namespace = term.getElementsByTagName("namespace")[0].firstChild.nodeValue #get the namespace
-    go_id = term.getElementsByTagName("id")[0].firstChild.nodeValue # get the id
-    name = term.getElementsByTagName("name")[0].firstChild.nodeValue# get the name
-    is_a_list = term.getElementsByTagName("is_a") # get the all is_a
-    count=len(is_a_list) # count the amount of is_a
-    if namespace in ontology_max and count > ontology_max[namespace][2]:# choose the biggest amount of is_a of the term 
-         ontology_max[namespace] = (go_id, name, count)
-print("Results from DOM parser:")
-for ns, (go_id, name, count) in ontology_max.items():# print the results
-    print(f"Ontology: {ns}")
-    print(f"  GO ID: {go_id}")
-    print(f"  Name: {name}")
-    print(f"  Number of <is_a>: {count}")
 
-end_time = datetime.now()
-dom_duration = end_time - start_time
-print(f"Time taken to parse XML using DOM: {dom_duration} seconds")
+max_values = {
+    "molecular_function": 0,
+    "biological_process": 0,
+    "cellular_component": 0
+}
 
-#SAX
-class GOHandler(xml.sax.ContentHandler):# create a class to handle the SAX parsing
-    def __init__(self):# initialize the class
-        self.current_element = "" #set the current element to empty
-        self.go_id = "" #set the go_id to empty
-        self.name = "" #set the name to empty
-        self.namespace = ""# set the namespace to empty
-        self.is_a_count = 0# set the is_a_count to 0
-        # set the max_counts dictionary to store the maximum counts for each namespace
-        self.max_counts = {
-            "molecular_function": ("", "", 0),
-            "biological_process": ("", "", 0),
-            "cellular_component": ("", "", 0)
+# Count the maximum "is_a" ​for terms in each namespace using ​DOM.
+for current_term in all_terms:
+    ns = current_term.getElementsByTagName("namespace")[0].firstChild.nodeValue
+    identifier = current_term.getElementsByTagName("id")[0].firstChild.nodeValue
+    term_name = current_term.getElementsByTagName("name")[0].firstChild.nodeValue
+    parent_relations = current_term.getElementsByTagName("is_a")
+    relation_count = len(parent_relations)
+    
+# Updates max is_a terms: replace if higher, append if equal.
+    if ns in ontology_results:
+        if relation_count > max_values[ns]:
+            ontology_results[ns] = [(identifier, term_name, relation_count)]
+            max_values[ns] = relation_count
+        elif relation_count == max_values[ns]:
+            ontology_results[ns].append((identifier, term_name, relation_count))
+
+print("DOM Parser Output:") # This classifys the output (like a title), which is clearer.
+for namespace, term_data in ontology_results.items():
+    print(f"Category: {namespace}")
+    for term_id, term_desc, term_count in term_data:
+        if term_count == max_values[namespace]:
+            print(f"  Identifier: {term_id}")
+            print(f"  Description: {term_desc}")
+            print(f"  Parent Relations: {term_count}\n")
+end_time = tm.time()
+dom_duration = end_time - begin_time
+print(f"DOM processing time: {dom_duration:.2f} seconds")
+
+# SAX parser set up
+class OntologyHandler(xs.ContentHandler):
+    def __init__(self):
+        self.current_tag = ""
+        self.term_id = ""
+        self.term_name = ""
+        self.term_ns = ""
+        self.parent_count = 0
+        self.results = {
+            "molecular_function": [],
+            "biological_process": [],
+            "cellular_component": []
         }
-        # set the temp variables to empty
-        self.temp_name = ""
-        self.temp_namespace = ""
-        self.temp_id = ""
+        self.max_counts = {
+            "molecular_function": 0,
+            "biological_process": 0,
+            "cellular_component": 0
+        }
+        self.temp_data = {
+            "name": "",
+            "ns": "",
+            "id": ""
+        }
         self.in_term = False
-        self.in_name = False
+        self.processing_name = False
 
-    def startElement(self, tag, attributes):# set the start element
-        self.current_element = tag# set the current element to the tag
+    def startElement(self, tag, attrs):
+        self.current_tag = tag
         if tag == "term":
-            self.in_term = True# set the in_term to True
-            self.go_id = "" # set the go_id to empty
-            self.name = "" # set the name to empty
-            self.namespace = "" # set the namespace to empty
-            self.is_a_count = 0 # set the is_a_count to 0
-            self.temp_namespace = ""# set the temp_namespace to empty
-            self.temp_name = "" # set the temp_name to empty
-            self.temp_id = "" # set the temp_id to empty
-        if tag == "name":
-            self.in_name = True # set the in_name to True
+            self.in_term = True
+            self.term_id = ""
+            self.term_name = ""
+            self.term_ns = ""
+            self.parent_count = 0
+            self.temp_data = {"name": "", "ns": "", "id": ""}
+        elif tag == "name":
+            self.processing_name = True
+        elif self.current_tag == "is_a":
+            self.parent_count += 1
 
     def characters(self, content):
-        if not self.in_term: 
+        if not self.in_term:
             return
-        if self.current_element == "id":# check the if the current element is id
-            self.temp_id+= content
-        elif self.current_element == "namespace": # check the if the current element is namespace
-            self.temp_namespace += content
-        elif self.current_element == "is_a": # check the if the current element is is_a
-            self.is_a_count += 1
-        elif self.in_name: # check the if the in_name is True
-            self.temp_name += content
+        if self.current_tag == "id":
+            self.temp_data["id"] += content
+        elif self.current_tag == "namespace":
+            self.temp_data["ns"] += content
+        elif self.processing_name:
+            self.temp_data["name"] += content
 
     def endElement(self, tag):
         if tag == "namespace":
-            self.namespace = self.temp_namespace.strip()# set the namespace to temp_namespace
+            self.term_ns = self.temp_data["ns"].strip()
         elif tag == "id":
-            self.go_id = self.temp_id.strip()# set the go_id to temp_id
+            self.term_id = self.temp_data["id"].strip()
         elif tag == "name":
-            self.name = self.temp_name.strip()# set the name to temp_name
-            self.in_name = False
-        elif tag == "term":# update the max_counts dictionary
-            if self.namespace in self.max_counts and self.is_a_count > self.max_counts[self.namespace][2]:
-                self.max_counts[self.namespace] = (self.go_id, self.name, self.is_a_count)
-            self.in_term = False
+            self.term_name = self.temp_data["name"].strip()
+            self.processing_name = False
+        elif tag == "term":
+            if self.term_ns in self.results:
+                if self.parent_count > self.max_counts[self.term_ns]:
+                    self.results[self.term_ns] = [(self.term_id, self.term_name, self.parent_count)]
+                    self.max_counts[self.term_ns] = self.parent_count
+                elif self.parent_count == self.max_counts[self.term_ns]:
+                    self.results[self.term_ns].append((self.term_id, self.term_name, self.parent_count))
 
-# record start time
-start_time = datetime.now()
+# SAX execution
+start_time = tm.time()
+sax_parser = xs.make_parser()
+sax_parser.setFeature(xs.handler.feature_namespaces, False)
+handler = OntologyHandler()
+sax_parser.setContentHandler(handler)
+sax_parser.parse(r"C:/Users/wym/Desktop/wym/IBI1_2024-25/Practical14/go_obo.xml")
 
-# set up SAX parser
-parser = xml.sax.make_parser()  # create an XMLReader
-parser.setFeature(xml.sax.handler.feature_namespaces, False) # disable namespace handling
-# set content handler
-handler = GOHandler()# create an instance of the handler
-parser.setContentHandler(handler)
-# parse the XML file
-parser.parse(xml_file)
-print("Results from SAX parser:")
-for ns, (go_id, name, count) in handler.max_counts.items():
-    print(f"Ontology: {ns}")
-    print(f"  GO ID: {go_id}")
-    print(f"  Name: {name}")
-    print(f"  Number of <is_a>: {count}")
-# record end time
-end_time = datetime.now()
+print("SAX Parser Output:")
+for ns, terms in handler.results.items():
+    print(f"Category: {ns}")
+    for t_id, t_name, t_count in terms:
+        if t_count == handler.max_counts[ns]:
+            print(f"  Identifier: {t_id}")
+            print(f"  Description: {t_name}")
+            print(f"  Parent Relations: {t_count}\n")
+
+end_time = tm.time()
 sax_duration = end_time - start_time
-print(f"SAX parser took {sax_duration} seconds.\n") # print time taken for SAX 
+print(f"SAX processing time: {sax_duration:.2f} seconds")
 
-# compare rate
+# Rate comparison
 if sax_duration < dom_duration:
-    print("# SAX was faster.")
+    print("SAX was the quickest")
 else:
-    print("# DOM was faster.")
-# Comment: SAX is faster
+    print("DOM was the quickest")
+# Comment: SAX was the quickest
