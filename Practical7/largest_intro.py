d4eef9f32a408d7afda11eaf5e73854b1f3a6702
yuming333 <yuming.24@intl.zju.edu.cn>
2025-05-28 10:07:58 +0800

    Update largest_intro.py

--- Practical7/largest_intro.py
+++ Practical7/largest_intro.py
@@ -3,6 +3,13 @@ import re
 # Define the variable seq
 seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
 intro_seqs = re.findall(r'GT.*AG',seq) # Extract the specific intron sequence from seq
-longest_seq = intro_seqs[0]
-max_length = len(longest_seq) # Calculate the length
-print(f"The longest intron sequence is '{longest_seq}', whose length is {max_length}.") # Use f-string to print the results as a sentence, inserting the variable into the strings while avoiding making grammar mistakes
+
+if not intro_seqs:
+    print("No intron sequences matching the GT...AG pattern were detected.") 
+# Ensure the code still works when the variable seq is modified.
+
+else:
+    longest_seq = intro_seqs[0]
+    max_length = len(longest_seq) # Calculate the length
+print(f"The longest intron sequence is '{longest_seq}', whose length is {max_length}.") 
+# Use f-string to print the results as a sentence, inserting the variable into the strings while avoiding making grammar mistakes
